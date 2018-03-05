86193fda15328be28933396a60eefcde39917a53
P10363830 <P10363830@MATMITLT1046>
2018-03-05 10:59:57 +0000

    :)

--- wfaBasicInputControls/Form1.Designer.cs
+++ wfaBasicInputControls/Form1.Designer.cs
@@ -28,12 +28,75 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.labelName = new System.Windows.Forms.Label();
+            this.cboName = new System.Windows.Forms.ComboBox();
+            this.btnAddToList = new System.Windows.Forms.Button();
+            this.lstNames = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Location = new System.Drawing.Point(12, 9);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(35, 13);
+            this.labelName.TabIndex = 0;
+            this.labelName.Text = "Name";
+            // 
+            // cboName
+            // 
+            this.cboName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cboName.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.cboName.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.cboName.FormattingEnabled = true;
+            this.cboName.Location = new System.Drawing.Point(53, 6);
+            this.cboName.Name = "cboName";
+            this.cboName.Size = new System.Drawing.Size(149, 21);
+            this.cboName.TabIndex = 1;
+            // 
+            // btnAddToList
+            // 
+            this.btnAddToList.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnAddToList.Location = new System.Drawing.Point(217, 4);
+            this.btnAddToList.Name = "btnAddToList";
+            this.btnAddToList.Size = new System.Drawing.Size(75, 23);
+            this.btnAddToList.TabIndex = 2;
+            this.btnAddToList.Text = "Add";
+            this.btnAddToList.UseVisualStyleBackColor = true;
+            this.btnAddToList.Click += new System.EventHandler(this.btnAddToList_Click);
+            this.btnAddToList.KeyDown += new System.Windows.Forms.KeyEventHandler(this.btnAddToList_KeyDown);
+            // 
+            // lstNames
+            // 
+            this.lstNames.FormattingEnabled = true;
+            this.lstNames.Location = new System.Drawing.Point(53, 33);
+            this.lstNames.Name = "lstNames";
+            this.lstNames.Size = new System.Drawing.Size(158, 95);
+            this.lstNames.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(312, 272);
+            this.Controls.Add(this.lstNames);
+            this.Controls.Add(this.btnAddToList);
+            this.Controls.Add(this.cboName);
+            this.Controls.Add(this.labelName);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.ComboBox cboName;
+        private System.Windows.Forms.Button btnAddToList;
+        private System.Windows.Forms.ListBox lstNames;
     }
 }
 
--- wfaBasicInputControls/Form1.cs
+++ wfaBasicInputControls/Form1.cs
@@ -16,5 +16,27 @@ namespace wfaBasicInputControls
         {
             InitializeComponent();
         }
+
+        private void btnAddToList_Click(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrEmpty(cboName.Text))
+            {
+                lstNames.Items.Add(cboName.Text);
+                cboName.Items.Add(cboName.Text);
+                
+
+            }
+        }
+
+        private void btnAddToList_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (!String.IsNullOrEmpty(cboName.Text))
+            {
+                lstNames.Items.Add(cboName.Text);
+                cboName.Items.Add(cboName.Text);
+
+
+            }
+        }
     }
 }
